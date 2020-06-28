f7ed271646bc5fc8c42984931241cd6432dc33d5
Subrahmanyam G <subbuklce>
2020-06-28 12:39:34 +0530

    This is first project docs.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by subrahmanyam_g on 6/28/20.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    cout<<"\n This is a first program";
+
+    return 0;
+}
